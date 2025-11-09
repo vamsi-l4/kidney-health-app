3d65858387e690f2d8053e8bac08cebb359a0d22
vamsi-l4 <krishnavamsi3400@gmail.com>
2025-11-09 15:03:14 +0530

    updating the CORS origins in server/app/main.py to include the Vercel domain

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ if not logger.hasHandlers():
 # ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Specific origin for development
+    allow_origins=["http://localhost:5173", "https://kidneystone-blond.vercel.app"],  # Specific origins for development and production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
